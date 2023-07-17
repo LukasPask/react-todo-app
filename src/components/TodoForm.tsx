471e8f18af50df72c42f9709e251a6c5a62f1933
Lukas_x270 <lukas_x270@pop-os.localdomain>
2023-07-17 17:13:12 +0300

    Added inputs and form functionality

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const TodoForm = () => {
+  const { register, handleSubmit, control } = useForm();
+
+  const onSubmit = (data: object) => {
+    console.log(data);
+  };
+
   return (
-    <>
-      <TextField></TextField>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField label='Todo title' {...register('todoTitle')} />
+      <Controller
+        name='todoDate'
+        control={control}
+        defaultValue={new Date()}
+        render={({ field: { onChange, value } }) => (
+          <ReactDatePicker
+            showTimeSelect
+            timeFormat='p'
+            timeIntervals={15}
+            dateFormat='Pp'
+            selected={value}
+            onChange={(date) => onChange(date)}
+            dropdownMode='select'
+          />
+        )}
+      />
+      <Button type='submit' variant='contained'>
+        Add Todo
+      </Button>
+    </form>
   );
 };
 
