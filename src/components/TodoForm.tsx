2c32cd2668982299a1bb4f71f1308178b7d509db
Lukas_x270 <lukas_x270@pop-os.localdomain>
2023-07-26 19:48:08 +0300

    Added functionality for saving todos in localstorage

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,18 +1,41 @@
 import { Button, TextField } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
+import { v4 as uuidv4 } from 'uuid';
 import ReactDatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const TodoForm = () => {
-  const { register, handleSubmit, control } = useForm();
+  const { register, handleSubmit, control, reset } = useForm();
 
   const onSubmit = (data: object) => {
-    console.log(data);
+    reset();
+    const newTodoBody = {
+      ...data,
+      isCompleted: false,
+      id: uuidv4(),
+    };
+    const todosFromLocalStorage =
+      JSON.parse(localStorage.getItem('todos')!) ?? [];
+    localStorage.setItem(
+      'todos',
+      JSON.stringify([...todosFromLocalStorage, newTodoBody])
+    );
+    toast.success('Todo added successfully', {
+      position: 'bottom-left',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: 'colored',
+    });
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField label='Todo title' {...register('todoTitle')} />
+      <TextField required label='Todo title' {...register('todoTitle')} />
       <Controller
         name='todoDate'
         control={control}
@@ -22,7 +45,7 @@ const TodoForm = () => {
             showTimeSelect
             timeFormat='p'
             timeIntervals={15}
-            dateFormat='Pp'
+            dateFormat='yyyy-MM-dd h:mm aa'
             selected={value}
             onChange={(date) => onChange(date)}
             dropdownMode='select'
